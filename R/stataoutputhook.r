7680f2fc7f74aa418fdbbbb84e16538926aa221b
maoke <maoke94@gmail.com>
2023-01-28 14:31:17 -0500

    Formatted properly

--- R/stataoutputhook.r
+++ R/stataoutputhook.r
@@ -1,11 +1,17 @@
 stataoutputhook <- function(x, options) {
-<<<<<<< HEAD
-  if (options$noisey == TRUE)
+  if (options$noisey == TRUE) {
     message(paste("\n", options$engine, "output from chunk", options$label))
-  # print("input to stataoutputhook")
-  # print(x)
+    message("input to stataoutputhook()")
+    message(x)
+  }
+  x_noprofile <-
+    sub(
+      "^.*[R|r]unning[[:space:]].*p(\\\n>[[:space:]])?r(\\\n>[[:space:]])?o(\\\n>[[:space:]])?f(\\\n>[[:space:]])?i(\\\n>[[:space:]])?l(\\\n>[[:space:]])?e(\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?d(\\\n>[[:space:]])?o(\\\n>[[:space:]])?[[:space:]](\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?\\.[[:space:]]?[[:space:]]?",
+      "",
+      x
+    )
   if (options$engine == "stata") {
-    y <- strsplit(x, "\n")[[1]]
+    y <- strsplit(x_noprofile, "\n")[[1]]
     # print("input to stata output parse")
     # print(y)
     # Remove "running profile.do"
@@ -28,31 +34,6 @@ stataoutputhook <- function(x, options) {
         # loopcommands <- grep("^[[:space:]][[:space:]][[:digit:]+]\\.", y)
         loopcommands <- grep("^[[:space:]]+[[:digit:]]+\\.", y)
         if (length(commandlines) > 0 && length(loopcommands) > 0) {
-=======
-    if (options$noisey==TRUE) {
-      message(paste("\n", options$engine, "output from chunk", options$label))
-      message("input to stataoutputhook()")
-      message(x)
-      }
-    x_noprofile <- sub("^.*[R|r]unning[[:space:]].*p(\\\n>[[:space:]])?r(\\\n>[[:space:]])?o(\\\n>[[:space:]])?f(\\\n>[[:space:]])?i(\\\n>[[:space:]])?l(\\\n>[[:space:]])?e(\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?d(\\\n>[[:space:]])?o(\\\n>[[:space:]])?[[:space:]](\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?\\.(\\\n>[[:space:]])?\\.[[:space:]]?[[:space:]]?", "", x)
-    if (options$engine=="stata") {
-      y <- strsplit(x_noprofile, "\n")[[1]]
-# print("input to stata output parse")
-# print(y)
-# Remove "running profile.do"
-running <- grep("^\\.?[[:space:]]?[R|r]unning[[:space:]].*profile.do", y)
-if (length(running)>0) {y[running] <- sub("^\\.?[[:space:]]?[R|r]unning[[:space:]].*profile.do","", y[running])}
-     # print("running removed")
-# print(y)
-      # Remove command echo in Stata log
-      if (length(options$cleanlog)==0 | options$cleanlog!=FALSE) {
-        commandlines <- grep("^\\.[[:space:]]", y)
-# print(commandlines)
-        if (length(commandlines)>0) {
-          # loopcommands <- grep("^[[:space:]][[:space:]][[:digit:]+]\\.", y)
-          loopcommands <- grep("^[[:space:]]+[[:digit:]]+\\.", y)
-          if (length(commandlines)>0 && length(loopcommands)>0) {
->>>>>>> 1f51d359633f6b366df659507dd6b102d5b266f7
           for (i in 1:length(loopcommands)) {
             if ((loopcommands[i] - 1) %in% commandlines) {
               commandlines <- c(commandlines, loopcommands[i])
